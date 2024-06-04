37be1bfb918da60731791103c3a4eb91cd57bea4
SambaranFST <sambacha@in.ibm.com>
2024-06-04 17:47:09 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity3.java
@@ -0,0 +1,51 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
+
+public class Activity3 {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+        //Open browser
+        driver.get("https://v1.training-support.net/selenium/login-form");
+    }
+
+    @Test
+    public void loginTest() {
+        //Find the username and password fields
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        //Enter credentials
+        username.sendKeys("admin");
+        password.sendKeys("password");
+
+        //Click login
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+        //Read login message
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals("Welcome Back, admin", loginMessage);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //Close browser
+        driver.close();
+    }
+}
